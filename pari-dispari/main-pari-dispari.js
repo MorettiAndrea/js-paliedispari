983b8eb449084d7d0175bb3b2acfc66643559ea4
MorettiAndrea <andrea.moretti1704@gmail.com>
2025-04-07 18:16:15 +0200

    fine giornata da correggere  pari e dispari

--- pari-dispari/main-pari-dispari.js
+++ pari-dispari/main-pari-dispari.js
@@ -6,6 +6,8 @@ console.log("Il numero del pc", pcNumber);
 
 const pariODispari = prompt("Scegli pari o dispari");
 const playerNumber = parseInt(prompt("Scegli un numero da 1 a 5"));
+
+console.log("Hai scelto", pariODispari);
 console.log("Il tuo Numero", playerNumber);
 
 if ((pcNumber + playerNumber) % 2 === 0 && pariODispari === "pari") {
@@ -16,4 +18,6 @@ if ((pcNumber + playerNumber) % 2 === 0 && pariODispari === "pari") {
   alert("Hai perso!!!");
 } else if ((pcNumber + playerNumber) % 2 !== 0 && pariODispari === "dispari") {
   alert("Hai Vinto!!!");
+} else {
+  alert("Ricontrolla gli input immessi");
 }
