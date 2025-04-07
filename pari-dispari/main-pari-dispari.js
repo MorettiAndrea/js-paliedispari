64abb24ec86273a889ae854909cbffef8d5c93b4
MorettiAndrea <andrea.moretti1704@gmail.com>
2025-04-07 17:50:14 +0200

    proseguimento pari e dispari

---
+++ pari-dispari/main-pari-dispari.js
@@ -0,0 +1,19 @@
+function pcNumber() {
+  const randomNumber = Math.floor(Math.random() * 5) + 1;
+  return randomNumber;
+}
+console.log("Il numero del pc", pcNumber);
+
+const pariODispari = prompt("Scegli pari o dispari");
+const playerNumber = parseInt(prompt("Scegli un numero da 1 a 5"));
+console.log("Il tuo Numero", playerNumber);
+
+if ((pcNumber + playerNumber) % 2 === 0 && pariODispari === "pari") {
+  alert("Hai vinto!!!");
+} else if ((pcNumber + playerNumber) % 2 !== 0 && pariODispari === "pari") {
+  alert("Hai perso!!!");
+} else if ((pcNumber + playerNumber) % 2 === 0 && pariODispari === "dispari") {
+  alert("Hai perso!!!");
+} else if ((pcNumber + playerNumber) % 2 !== 0 && pariODispari === "dispari") {
+  alert("Hai Vinto!!!");
+}
