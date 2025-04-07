32cdf57a75162acf519e60c755fb5566c70da9f6
MorettiAndrea <andrea.moretti1704@gmail.com>
2025-04-07 17:07:04 +0200

    fine palindromi

--- palindromi/main-palindromi.js
+++ palindromi/main-palindromi.js
@@ -1,1 +1,16 @@
-const 
+function reversedWord(word) {
+  let reversed = "";
+  for (let i = word.length - 1; i >= 0; i--) {
+    reversed += word[i];
+  }
+  return reversed;
+}
+const choosedWord = prompt("Inserisci una parola");
+console.log(choosedWord);
+console.log(reversedWord(choosedWord));
+
+if (choosedWord === reversedWord(choosedWord)) {
+  alert("La parola è palindroma");
+} else {
+  alert("La parola non è palindroma");
+}
